675b36eedcd86a7f212c327d07c6ad68d05fe41f
HackerJang <jiwujang@naver.com>
2020-09-06 17:25:45 +0900

    Added send_music()

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+@app.route('/api/v1/get_music', methods=['GET', 'POST'])
+def send_music():
+    if request.method == 'POST':
+        music_name = request.form.get['music_name']
+        print(music_name, type(music_name))
+        music_file = open(music_name + ".mp3", "rb")
+        data = music_file.read()
+        music_file.close()
+        return data
+    else:
+        return "kikk! Game Music API"
+
+
+if __name__ == '__main__':
+    app.run()
